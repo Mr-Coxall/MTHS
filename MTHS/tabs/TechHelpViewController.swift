a4130c134868c9f8486a4a56fb1fafc8a480a867
Patrick Coxall <patrickcoxall@patricks-mac-mini.local>
2016-08-13 09:02:37 -0400

    Fixed the tech help tab.
    The mail thing crashes in simulator, but this is supposedly normal!

--- MTHS/tabs/TechHelpViewController.swift
+++ MTHS/tabs/TechHelpViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import MessageUI
 
-class TechHelpViewController: UIViewController {
+class TechHelpViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
 
+    @IBOutlet weak var techHelpLabel: UILabel!
+    @IBOutlet weak var techHelpButton: UIButton!
+    
+    var defaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // check if you are currently logged in and have data stored locally
+        if let studentName = defaults.stringForKey("studentName") {
+            // logged in
+            print("Loged in as:" + studentName)
+            techHelpLabel.hidden = true
+            techHelpButton.hidden = false
+
+        } else {
+            // NOT logged in !!
+            techHelpLabel.hidden = false
+            techHelpButton.hidden = true
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +42,40 @@ class TechHelpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func techHelpButton(sender: AnyObject) {
+        // send tech help email
+        
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["help@mths.ca"])
+            mail.setSubject("Tech Help Request")
+            mail.setMessageBody("<b>I need help with ...</b>", isHTML: true)
+            presentViewController(mail, animated: true, completion: nil)
+        } else {
+            print("Cannot send mail")
+            // give feedback to the user
+        }
+
+    }
+    
+    // MARK: - MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        switch result.rawValue {
+        case MFMailComposeResultCancelled.rawValue:
+            print("Cancelled")
+        case MFMailComposeResultSaved.rawValue:
+            print("Saved")
+        case MFMailComposeResultSent.rawValue:
+            print("Sent")
+        case MFMailComposeResultFailed.rawValue:
+            print("Error: \(error?.localizedDescription)")
+        default:
+            break
+        }
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
